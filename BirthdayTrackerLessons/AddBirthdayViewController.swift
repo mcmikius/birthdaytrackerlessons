b8e82aa2291b722f9172a2fdfdcdf25210839d05
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-16 09:47:07 +0300

    CREATING BUTTONS AND SCREENS IN STORYBOARD

---
+++ BirthdayTrackerLessons/AddBirthdayViewController.swift
@@ -0,0 +1,36 @@
+//
+//  AddBirthdayViewController.swift
+//  BirthdayTrackerLessons
+//
+//  Created by Mykhailo Bondarenko on 7/16/19.
+//  Copyright © 2019 Mykhailo Bondarenko. All rights reserved.
+//
+
+import UIKit
+
+class AddBirthdayViewController: UIViewController {
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet var firstNameTextField: UITextField!
+    @IBOutlet var lastNameTextField: UITextField!
+    @IBOutlet var birthdatePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
