d899d03a32260502ce9f8f6eb04023575d628c63
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-16 15:04:05 +0300

    Removing notification

--- BirthdayTrackerLessons/BirthdaysTableViewController.swift
+++ BirthdayTrackerLessons/BirthdaysTableViewController.swift
@@ -75,6 +75,10 @@ class BirthdaysTableViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if birthdays.count > indexPath.row {
             let birthday = birthdays[indexPath.row]
+            if let identifier = birthday.birthdayId {
+                let center = UNUserNotificationCenter.current()
+                center.removePendingNotificationRequests(withIdentifiers: [identifier])
+            }
             let appDelegate = UIApplication.shared.delegate as! AppDelegate
             let context = appDelegate.persistentContainer.viewContext
             context.delete(birthday)
