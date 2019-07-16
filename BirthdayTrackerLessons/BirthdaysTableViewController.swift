d99667efff91b1329ff896ce6184f7adc069cbbe
Mykhailo Bondarenko <mcmikius@icloud.com>
2019-07-16 10:05:18 +0300

    Displaying birthdays in a table view

--- BirthdayTrackerLessons/BirthdaysTableViewController.swift
+++ BirthdayTrackerLessons/BirthdaysTableViewController.swift
@@ -26,23 +26,22 @@ class BirthdaysTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return birthdays.count
     }
 
-    /*
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCell(withIdentifier: "birthdayCellIdentifier", for:indexPath)
+        let birthday = birthdays[indexPath.row]
+        cell.textLabel?.text = birthday.firstName + " " + birthday.lastName
+        cell.detailTextLabel?.text = dateFormatter.string(from: birthday.birthdate)
+        
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
